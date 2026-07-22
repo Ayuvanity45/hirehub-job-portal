18c54ef0d476b7adf53662ea91c9854ea7278cbc
unknown <ayushbhumihar45@gmail.com>
2026-07-22 15:50:54 +0530

    Debug DB environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 console.log("=================================");
+console.log("Environment keys:", Object.keys(process.env).filter(k => k.startsWith("DB")));
 console.log("DB_HOST =", process.env.DB_HOST);
 console.log("DB_PORT =", process.env.DB_PORT);
 console.log("DB_NAME =", process.env.DB_NAME);
